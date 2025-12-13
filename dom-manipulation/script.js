c3bb9aa5c4eca4fab9a25137983e12b7f7051d15
Emmanuel <emmanuelbaraka572@gmail.com>
2025-12-13 10:16:17 +0300

    Added index.html and script.js file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -17,9 +17,17 @@ function createAddQuoteForm() {
  const text = document.getElementById("newQuoteText").value.trim();
   const category = document.getElementById("newQuoteCategory").value.trim();
 
-  if (text && category) {
+ if (text && category) {
     quotes.push({ text, category });
-    showRandomQuote(); // optionally show the new quote
+
+    // Create a new <p> element to show the quote
+    const quoteElement = document.createElement("p");
+    quoteElement.textContent = `"${text}" —${category}`;
+
+    const quoteDisplay = document.getElementById("quoteDisplay");
+    quoteDisplay.innerHTML = ""; // Clear previous
+    quoteDisplay.appendChild(quoteElement); // Append new
+
     document.getElementById("newQuoteText").value = "";
     document.getElementById("newQuoteCategory").value = "";
   } else {
@@ -28,7 +36,9 @@ function createAddQuoteForm() {
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
+
+
+document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("newQuote").addEventListener("click", showRandomQuote);
   document.getElementsByTagName("button")[1].addEventListener("click", createAddQuoteForm);
   showRandomQuote(); // Show one on load
